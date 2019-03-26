9179917fb204828eb70804d5194ac5309740ebe7
Ibrahim Umar <ibrahim.umar@imr.no>
2019-03-26 16:28:09 +0100

    Use Pugixml for C++ XML parsing

--- src/readNMDxml.cpp
+++ src/readNMDxml.cpp
@@ -6,27 +6,30 @@
 #include <map>
 #include <string>
 
-#define RAPIDXML_NO_STDLIB
-#define RAPIDXML_NO_EXCEPTIONS
-#include "rapidxml-1.13/rapidxml.hpp"
+#define PUGIXML_HEADER_ONLY
+#define PUGIXML_NO_EXCEPTIONS
+
+#include "pugixml/pugixml.hpp"
 #include <Rcpp.h>
 
-#if defined(RAPIDXML_NO_EXCEPTIONS)
+/*
+#if defined(PUGIXML_NO_EXCEPTIONS)
 void rapidxml::parse_error_handler(const char* what, void* where)
 {
     Rcpp::stop("Parse error(@%p): %s\n", where, what);
 }
 #endif
+*/
 
-void processNode(const rapidxml::xml_node<>* node, const std::vector<char*>& parentPrefix, const Rcpp::List& treeStruct, const Rcpp::List& tableHeaders, const Rcpp::NumericVector& prefixLens, std::map<std::string, int>& levelCtrs, Rcpp::List& ret) {
-	const char* root = node->name();
+void processNode(pugi::xml_node& node, const std::vector<const char*>& parentPrefix, const Rcpp::List& treeStruct, const Rcpp::List& tableHeaders, const Rcpp::NumericVector& prefixLens, std::map<std::string, int>& levelCtrs, Rcpp::List& ret) {
+	const char* root = node.name();
 
 	// Getting header keys
 	Rcpp::CharacterVector NodeKeys = tableHeaders[root];
 	Rcpp::CharacterMatrix tempRes = ret[root];
 
 	// Prefix
-	std::vector<char*> prefix;
+	std::vector<const char*> prefix;
 	prefix.resize(prefixLens[root]);
 
 	// Apply parent attributes to row and to children prefix
@@ -36,32 +39,32 @@ void processNode(const rapidxml::xml_node<>* node, const std::vector<char*>& par
 	}
 
 	// Getting attributes
-	for(const rapidxml::xml_attribute<>* a = node->first_attribute()
+	for(pugi::xml_attribute a = node.first_attribute()
 		; a
-		; a = a->next_attribute()
+		; a = a.next_attribute()
 	) {
-		Rcpp::CharacterVector NodeKey(a->name());
+		Rcpp::CharacterVector NodeKey(a.name());
 		Rcpp::IntegerVector col = match(NodeKey, NodeKeys);
 		//cout << col[0] <<endl;
 		if( col[0] > 0 ) {
 			int idx = col[0] - 1;
-			tempRes(levelCtrs[root], idx) = a->value();
-			prefix[idx] = a->value();
+			tempRes(levelCtrs[root], idx) = a.value();
+			prefix[idx] = a.value();
 		}
 	}
 	
 	// Getting elements
-	for(const rapidxml::xml_node<>* n = node->first_node()
+	for(pugi::xml_node n = node.first_child()
 		; n
-		; n = n->next_sibling()
+		; n = n.next_sibling()
 	) {
-		//cout << n->name() << n->value() << endl;
+		//cout << n.name() << n.value() << endl;
 		// For echousounder's sa records
 		Rcpp::CharacterVector NodeKey("");
-		if(n->name()[0] == '\0')
+		if(n.name()[0] == '\0')
 			NodeKey[0] = root;
 		else
-			NodeKey[0] = n->name();
+			NodeKey[0] = n.name();
 		Rcpp::IntegerVector col = match(NodeKey, NodeKeys);
 		if( col[0] > 0 ) {
 #ifdef DEBUG
@@ -70,7 +73,7 @@ void processNode(const rapidxml::xml_node<>* node, const std::vector<char*>& par
 			cout << tempRes.ncol() << endl;
 			cout << tempRes.nrow() << endl;
 #endif
-			tempRes(levelCtrs[root], col[0] - 1) = n->value();
+			tempRes(levelCtrs[root], col[0] - 1) = n.text().as_string();
 		} else {
 			processNode(n, prefix, treeStruct, tableHeaders, prefixLens, levelCtrs, ret);
 		}
@@ -84,31 +87,32 @@ void processNode(const rapidxml::xml_node<>* node, const std::vector<char*>& par
 Rcpp::List readNMDxmlCpp(Rcpp::CharacterVector inputFile, Rcpp::CharacterVector root, Rcpp::List treeStruct, Rcpp::List tableHeaders, Rcpp::NumericVector prefixLens, Rcpp::NumericVector levelDims)
 {
 
-	rapidxml::xml_document<> doc;
-	rapidxml::xml_node<> * root_node;
+	pugi::xml_document doc;
+	pugi::xml_node root_node;
 
 	Rcpp::Rcout << "Parsing biotic XML :" << inputFile << "." << std::endl;
 
-	// Read xml using ifstream and buffer vector
-	std::ifstream iFile (inputFile[0]);
-	std::vector<char> buffer((std::istreambuf_iterator<char>(iFile)), std::istreambuf_iterator<char>());
-	buffer.push_back('\0');
+ 	if (!doc.load_file(inputFile[0])) return -1;
 
-	// Parse the buffer using the xml file parsing library into doc 
-	doc.parse<0>(&buffer[0]);
+	// Read xml using ifstream and buffer vector
+	//std::ifstream iFile (inputFile[0]);
+	//std::vector<char> buffer((std::istreambuf_iterator<char>(iFile)), std::istreambuf_iterator<char>());
+	//buffer.push_back('\0');
+
+	//pugi::xml_parse_result result = doc.load_buffer_inplace_own(&buffer[0], buffer.size());
 
 	// Find our root node
 	char * rootStr = root[0];
-	root_node = doc.first_node(rootStr);
+	root_node = doc.child(rootStr);
 
 	// Create prefix storage
-	std::vector<char*> prefix;
+	std::vector<const char*> prefix;
 
 	// Allowing one level down
 	if(!root_node) {
 		Rcpp::CharacterVector downLevel = treeStruct[rootStr];
 		rootStr = downLevel[0];
-		root_node = doc.first_node(rootStr);
+		root_node = doc.child(rootStr);
 	}
 
 	// Prepare counters
