30a8f04aec3201a6f42ca1cc27b0e204ccf42979
Ibrahim Umar <ibrahim.umar@imr.no>
2019-06-12 12:23:04 +0200

    Add other biotic formats

--- R/data.R
+++ R/data.R
@@ -3,6 +3,10 @@
 #' @format A list with 4 elements
 #' \describe{
 #'   \item{\code{landingerv2.xsd}}{List Landing Format v2}
+#'   \item{\code{nmdbioticv1.xsd}}{List NMD Biotic Format v1}
+#'   \item{\code{nmdbioticv1.1.xsd}}{List NMD Biotic Format v1.1}
+#'   \item{\code{nmdbioticv1.2.xsd}}{List NMD Biotic Format v1.2}
+#'   \item{\code{nmdbioticv1.3.xsd}}{List NMD Biotic Format v1.3}
 #'   \item{\code{nmdbioticv1.4.xsd}}{List NMD Biotic Format v1.4}
 #'   \item{\code{nmdbioticv3.xsd}}{List NMD Biotic Format v3}
 #'   \item{\code{nmdechosounderv1.xsd}}{List NMD Echosounder Format v1}
