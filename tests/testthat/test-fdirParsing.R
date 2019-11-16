87c8a86ac195c2a6ac9b1fb69b2006f505bd853d
edvinf <edvin.fuglebakk@gmail.com>
2019-11-17 00:51:38 +0100

    Added test for reading biotic 3

--- tests/testthat/test-fdirParsing.R
+++ tests/testthat/test-fdirParsing.R
@@ -1,7 +1,7 @@
 context("test-fdirParsing")
 
 context("readLssFile: normal run")
-data <- readLssFile(system.file("testresources","landings_trimmed_2018.lss", package="RstoxFDA"))
+data <- readLssFile(system.file("testresources","landings_trimmed_2018.lss", package="RstoxData"))
 expect_true("Bruttovekt" %in% names(data))
 expect_true("Redskap" %in% names(data))
 expect_true("Snurpenot/ringnot" %in% data$Redskap)
