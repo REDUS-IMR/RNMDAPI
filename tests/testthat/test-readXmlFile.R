7843653dd3b2e9afba8a19082ab8a9fc74e5ecc7
edvinf <edvin.fuglebakk@gmail.com>
2019-11-17 00:54:52 +0100

    Updated biotic3 schema to be current with http://www.imr.no/formats/nmdbiotic/v3/nmdbioticv3.xsd

--- tests/testthat/test-readXmlFile.R
+++ tests/testthat/test-readXmlFile.R
@@ -2,5 +2,5 @@ context("test-readXmlFile")
 
 example <- system.file("testresources","biotic_v3_example.xml", package="RstoxData")
 defaultParse <- readXmlFile(example)
-expect_true(c("mission", "fishstation", "catchsample", "individual", "age") %in% names(defaultParse))
+expect_true(all(c("mission", "fishstation", "catchsample", "individual", "agedetermination") %in% names(defaultParse)))
 expect_equal(nrow(defaultParse$fishstation), 2)
