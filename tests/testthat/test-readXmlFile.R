cd61effdcb925d79d655f6c8efbecca1d55a7ffc
edvinf <edvin.fuglebakk@gmail.com>
2019-11-21 20:24:40 +0100

    Added test for NMDlanding

--- tests/testthat/test-readXmlFile.R
+++ tests/testthat/test-readXmlFile.R
@@ -1,12 +1,18 @@
 
-context("test-readXmlFile: DOM")
+context("test-readXmlFile: DOM biotic")
 example <- system.file("testresources","biotic_v3_example.xml", package="RstoxData")
 defaultParse <- readXmlFile(example, stream = F)
 expect_true(all(c("mission", "fishstation", "catchsample", "individual", "agedetermination") %in% names(defaultParse)))
 expect_equal(nrow(defaultParse$fishstation), 2)
 
 
-context("test-readXmlFile: stream parse")
+context("test-readXmlFile: stream parse biotic")
 streamParse <- readXmlFile(example, stream = T)
 expect_true(all(c("mission", "fishstation", "catchsample", "individual", "agedetermination") %in% names(streamParse)))
 expect_equal(nrow(defaultParse$fishstation), 2)
+
+context("test-readXmlFile: stream parse landing")
+example <- system.file("testresources","landing.xml", package="RstoxData")
+streamParse <- readXmlFile(example, stream = T)
+expect_true(all(c("Art", "Dellanding", "Fangstdata", "Landingsdata", "Seddellinje") %in% names(streamParse)))
+expect_false(any(is.na(streamParse$Produkt$Rundvekt)))
