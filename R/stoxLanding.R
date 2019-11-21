95d7cefa4446646dfc6d209356a0b4f6a127f3c4
edvinf <edvin.fuglebakk@gmail.com>
2019-11-21 22:40:40 +0100

    Draft for StoxLanding definitions

---
+++ R/stoxLanding.R
@@ -0,0 +1,59 @@
+#' StoxLandingData
+#'
+#' Table (\code{\link[data.table]{data.table}}) with aggregated landings data from sales notes.
+#'
+#'
+#' @section Column definitions:
+#'  \describe{
+#'   \item{speciesFAOCommercial}{FAO code for species (ASFIS)}
+#'   \item{speciesCategoryCommercial}{code for species category (several codes may code the same species or stock, and some species may be recorded only at higher taxonomic classifications)}
+#'   \item{commonNameCommercial}{common name used for species category in trade documents}
+#'   \item{weight}{Weight of round catch in kg. Round weight may be estimated from post-processing weights.}
+#'   \item{year}{Year of catch}
+#'   \item{catchDate}{Date of catch (last catch on trip)}
+#'   \item{gear}{Code for gear used for catch (dominant gear for trip)}
+#'   \item{gearDescription}{Descriptive text for column 'gear'}
+#'   \item{area}{Area code for the position of catch (dominant area for trip)}
+#'   \item{location}{Location code (subdivision of 'Area') for the position of catch (dominant area for trip)}
+#'   \item{icesAreaGroup}{Area code for the position of catch (dominant area for trip), based on different levels of the ICES spatial coding system}
+#'   \item{coastal}{code indidcating whether catch was taken within coastal delimitation line (dominant side for trip)}
+#'   \item{coastalDescription}{Descriptive text for column 'coastal'}
+#'   \item{n62Description}{Descriptive text indidcating whether catch was taken north or south of 62 deg. Lat. (dominant side for trip)}
+#'   \item{vesselLength}{Maximal length of vessel in meters}
+#'   \item{countryVessel}{Country of the vessel that caugth the catch}
+#'   \item{landingSite}{Code identifying landing site (buyer of catch)}
+#'   \item{countryLanding}{Country where catch was landed}
+#'   \item{usage}{Market usage of catch.}
+#'   \item{usageDescription}{Descriptive text for column 'usage'}
+#'  }
+#'  
+#' @section Correspondance to other formats:
+#'  Correspondances indicate which field a value is derived from, not necessarily verbatim copied.
+#' 
+#'  Correspondance to NMDlandings (http://www.imr.no/formats/landinger/v2):
+#'  \describe{
+#'   \item{speciesFAOCommercial}{ArtFAO_kode}
+#'   \item{speciesCategoryCommercial}{Art_kode}
+#'   \item{commonNameCommercial}{Art_bokmål}
+#'   \item{weight}{Rundvekt}
+#'   \item{year}{Fangstår}
+#'   \item{catchDate}{SisteFangstdato}
+#'   \item{gear}{Redskap_kode}
+#'   \item{gearDescription}{Redskap_bokmål}
+#'   \item{area}{Hovedområde_kode}
+#'   \item{location}{Lokasjon_kode}
+#'   \item{icesAreaGroup}{Områdegruppering_bokmål}
+#'   \item{coastal}{KystHav_kode}
+#'   \item{coastalDescription}{KystHav_kode}
+#'   \item{n62}{NordSørFor62GraderNord}
+#'   \item{vesselLength}{StørsteLengde}
+#'   \item{countryVessel}{Fartøynasjonalitet_kode}
+#'   \item{landingSite}{Mottaksstasjon}
+#'   \item{countryLanding}{Landingsnasjon_kode}
+#'   \item{usage}{HovedgruppeAnvendelse_kode}
+#'   \item{usageDescription}{HovedgruppeAnvendelse_bokmål}
+#'  }
+#'
+#' @name StoxLandingData
+#'
+NULL
